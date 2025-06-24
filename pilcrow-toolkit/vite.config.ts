bb4bc78c21cf69a0cd8958c92a7a0c3e9adc1f82
Brian Adams <wreality@gmail.com>
2025-06-24 17:50:45 -0400

    update

--- pilcrow-toolkit/vite.config.ts
+++ pilcrow-toolkit/vite.config.ts
@@ -3,32 +3,46 @@ import { builtinModules } from "node:module";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
-const external = [...builtinModules, ...builtinModules.map((m) => `node:${m}`)];
+const allCoreModules = builtinModules.flatMap((moduleName) => [
+  moduleName,
+  `node:${moduleName}`,
+]);
+
+const globalsForAllCoreModules = allCoreModules.reduce(
+  (acc, moduleName) => {
+    const [prefix, namePart] = moduleName.split(":");
+    acc[moduleName] = prefix === "node" ? namePart : moduleName;
+    return acc;
+  },
+  {} as Record<string, string>
+);
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            "/src": resolve(__dirname, "src"),
-            "/commands": resolve(__dirname, "src/commands"),
-            "/lib": resolve(__dirname, "src/lib"),
-        },
+  resolve: {
+    alias: {
+      "/src": resolve(__dirname, "src"),
+      "/commands": resolve(__dirname, "src/commands"),
+      "/lib": resolve(__dirname, "src/lib"),
     },
-    build: {
-        sourcemap: true,
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "src/main.ts"),
-                post: resolve(__dirname, "src/post.ts"),
-                // pre: resolve(__dirname, "src/pre.ts"),
-            },
-            external,
-            output: [
-                {
-                    format: "es",
-                    entryFileNames: "[name].js",
-                },
-            ],
-            preserveSymlinks: true,
+  },
+  build: {
+    sourcemap: true,
+    target: "node20",
+    rollupOptions: {
+      external: [...allCoreModules],
+      input: {
+        main: resolve(__dirname, "src/main.ts"),
+        post: resolve(__dirname, "src/post.ts"),
+        // pre: resolve(__dirname, "src/pre.ts"),
+      },
+      output: [
+        {
+          format: "es",
+          entryFileNames: "[name].js",
+          globals: globalsForAllCoreModules,
         },
+      ],
+      preserveSymlinks: true,
     },
+  },
 });
