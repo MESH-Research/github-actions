c533a7417e61e6f9c28b16ed6f443e7955710eec
Brian Adams <wreality@gmail.com>
2025-06-24 12:19:32 -0400

    update

--- pilcrow-toolkit/src/main.ts
+++ pilcrow-toolkit/src/main.ts
@@ -1,13 +1,12 @@
 import { getInput } from "@actions/core";
 import { run } from "/src/action";
 
+console.log(process.env);
 const inputs = {
   "docker-metadata": getInput("docker-metadata"),
   "bake-files": getInput("bake-files"),
   target: getInput("target"),
   command: getInput("command", { required: true }),
 };
-
-console.log(process.env);
 
 run("main", inputs);
